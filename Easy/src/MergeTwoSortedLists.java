036d05abc12fabc63e0fcb0c0818da2dffcdf781
datphvn <ecmapdit@gmail.com>
2025-09-19 15:58:08 +0700

    Fix MergeTwoSortedLists

--- Easy/src/MergeTwoSortedLists.java
+++ Easy/src/MergeTwoSortedLists.java
@@ -1,3 +1,4 @@
+// Định nghĩa ListNode
 class ListNode {
     int val;
     ListNode next;
@@ -6,42 +7,54 @@ class ListNode {
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
 
-class MergeTwoSortedLists {
+// Giải pháp chính
+class Solution1 {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(-1);
-        ListNode current = dummy;
+        ListNode cur = dummy;
 
         while (list1 != null && list2 != null) {
             if (list1.val <= list2.val) {
-                current.next = list1;
+                cur.next = list1;
                 list1 = list1.next;
             } else {
-                current.next = list2;
+                cur.next = list2;
                 list2 = list2.next;
             }
-            current = current.next;
+            cur = cur.next;
         }
 
-        if (list1 != null) current.next = list1;
-        if (list2 != null) current.next = list2;
+        // nối phần còn lại
+        cur.next = (list1 != null) ? list1 : list2;
 
         return dummy.next;
     }
+}
 
-    // Test main
+// Class chạy thử
+public class MergeTwoSortedLists {
     public static void main(String[] args) {
-        // list1 = []
-        ListNode list1 = null;
-        // list2 = [0]
-        ListNode list2 = new ListNode(0);
+        // Tạo list1: [1,2,4]
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
 
-        MergeTwoSortedLists solver = new MergeTwoSortedLists();
-        ListNode merged = solver.mergeTwoLists(list1, list2);
+        // Tạo list2: [1,3,4]
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        // Gọi hàm
+        Solution1 sol = new Solution1();
+        ListNode merged = sol.mergeTwoLists(list1, list2);
 
         // In kết quả
-        while (merged != null) {
-            System.out.print(merged.val + " ");
-            merged = merged.next;
+        printList(merged);  // Output: 1 -> 1 -> 2 -> 3 -> 4 -> 4
+    }
+
+    // Hàm in danh sách
+    private static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+            if (head.next != null) System.out.print(" -> ");
+            head = head.next;
         }
+        System.out.println();
     }
 }
