4c81f66b195eae83bc65ffa6b4792a2fce1f4a77
datphvn <ecmapdit@gmail.com>
2025-09-24 12:13:23 +0700

    Add RemoveDuplicatesFromSortedList

---
+++ Easy/src/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,18 @@
+class RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next; // bỏ node trùng
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+}
